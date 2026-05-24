160178b6a51b4dffeda3ff4b793618ba42448d3d
AnhKhoi <khoina231@gmail.com>
2026-05-24 12:50:34 +0700

    init entity, utils pkg

---
+++ backend/src/main/java/com/kna/backend/entity/Block.java
@@ -0,0 +1,22 @@
+package com.kna.backend.entity;
+
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data, String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
+
+
+
+
+
+}
